2aac36411be654a948391df6a867d054fadecd9b
Tup Putthi <tup47257@gmail.com>
2023-01-13 09:42:21 +0700

    quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,159 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchQuizQuestions } from "./API";
+// components
+import QuestionCard from "./components/QuestionCard";
+// Types
+import { QuestionState, Difficulty } from "./API";
+// styles
+import { GlobalStyle, Wrapper } from "./App.styles";
+import { Box, FormControl, Input, styled, TextField } from "@mui/material";
 
-function App() {
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const QuestionNumberContainer = styled(Box)({
+  maxWidth: 1100,
+  background: "#ebfeff",
+  borderRadius: 10,
+  border: "2px solid #0085a3",
+  padding: 20,
+  boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.25)",
+  textAlign: "center",
+});
+
+const App = () => {
+  const [totalQuestions, setTotalQuestions] = useState<number>(10);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [quizOver, setQuizOver] = useState(true);
+
+  const startQuiz = async () => {
+    setLoading(true);
+    setQuizOver(false);
+
+    const newQuestions = await fetchQuizQuestions();
+    console.log("newQuestions", newQuestions);
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!quizOver) {
+      //users answer
+      const answer = e.currentTarget.value;
+      // check answer agianst correct answer
+      const correct = questions[number].correct_answer === answer;
+      if (correct) {
+        setScore((currentScore) => currentScore + 1);
+      }
+      // save answer in the array gor user answer
+      const AnswerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((currentAnswers) => [...currentAnswers, AnswerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    // move on to the next question if not the last question
+    const nextQuestion = number + 1;
+    if (Number(nextQuestion) === Number(totalQuestions)) {
+      setQuizOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
+
+  const changeQuestionsNumber = (questionNumber: any) => {
+    setTotalQuestions(questionNumber);
+  };
+
+  const resetQuestion = (questionNumber: any) => {
+    setTotalQuestions(10);
+    setLoading(false);
+    setQuestions([]);
+    setNumber(0);
+    setUserAnswers([]);
+    setScore(0);
+    setQuizOver(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>React Quiz</h1>
+        {/* {quizOver ? (
+          <QuestionNumberContainer>
+            <TextField
+              required
+              id="outlined-required"
+              label="Questions Amount"
+              type="number"
+              error={!totalQuestions || totalQuestions > 20}
+              helperText="Questions must be less than 20"
+              defaultValue="10"
+              onChange={(e) => changeQuestionsNumber(e.target.value)}
+            />
+          </QuestionNumberContainer>
+        ) : null} */}
+        {!quizOver ? <p className="score">Score: {score}</p> : null}
+        {/* {quizOver || Number(userAnswers.length) === Number(totalQuestions) ? (
+          <button
+            disabled={!totalQuestions || totalQuestions > 20}
+            className="start"
+            onClick={resetQuestion}
+          >
+            Reset
+          </button>
+        ) : null} */}
+        {loading && <p>Loading Questions ...</p>}
+        {!loading && !quizOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={totalQuestions}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!quizOver &&
+        !loading &&
+        userAnswers.length === number + 1 &&
+        number !== totalQuestions - 1 ? (
+          <button className="next" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+
+        {quizOver || Number(userAnswers.length) === Number(totalQuestions) ? (
+          <button
+            disabled={!totalQuestions || totalQuestions > 20}
+            className="start"
+            onClick={startQuiz}
+          >
+            {!quizOver ? "start agian" : "start"}
+          </button>
+        ) : null}
+      </Wrapper>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/page/Home.tsx
@@ -0,0 +1,127 @@
+import { Box, Button } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
+import { AnswerObject } from "../App";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionContainer = styled(Box)({
+  maxWidth: 1100,
+  background: "#ebfeff",
+  borderRadius: 10,
+  border: "2px solid #0085a3",
+  padding: 20,
+  boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.25)",
+  textAlign: "center",
+});
+
+const ButtonAnswer = styled(Button)({
+  cursor: "pointer",
+  userSelect: "none",
+  fontSize: "0.8rem",
+  width: "100%",
+  height: 40,
+  background: "linear-gradient(90deg, #56ccff, #6eafb4)",
+  border: "2px solid #fff",
+  boxShadow: "1px 2px 0px rgba(0, 0, 0, 0.1)",
+  borderRadius: 10,
+  padding: "0 40px",
+  color: "#ffffff",
+});
+const ButtonCorrectAnswer = styled(Button)({
+  cursor: "pointer",
+  userSelect: "none",
+  fontSize: "0.8rem",
+  width: "100%",
+  height: 40,
+  background: "linear-gradient(90deg, #56ffa4, #59bc86)",
+  border: "2px solid #fff",
+  boxShadow: "1px 2px 0px rgba(0, 0, 0, 0.1)",
+  borderRadius: 10,
+  padding: "0 40px",
+  color: "#ffffff",
+});
+
+const ButtonInCorrectAnswer = styled(Button)({
+  cursor: "pointer",
+  userSelect: "none",
+  fontSize: "0.8rem",
+  width: "100%",
+  height: 40,
+  background: "linear-gradient(90deg, #ff5656, #c16868)",
+  border: "2px solid #fff",
+  boxShadow: "1px 2px 0px rgba(0, 0, 0, 0.1)",
+  borderRadius: 10,
+  padding: "0 40px",
+  color: "#ffffff",
+});
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <QuestionContainer>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div key={answer}>
+          {userAnswer?.correctAnswer === answer && (
+            <ButtonCorrectAnswer
+              style={{
+                marginTop: "5px",
+                marginBottom: "5px",
+              }}
+              disabled={userAnswer ? true : false}
+              value={answer}
+              onClick={callback}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </ButtonCorrectAnswer>
+          )}
+          {userAnswer?.answer === answer && (
+            <ButtonInCorrectAnswer
+              style={{
+                marginTop: "5px",
+                marginBottom: "5px",
+              }}
+              disabled={userAnswer ? true : false}
+              value={answer}
+              onClick={callback}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </ButtonInCorrectAnswer>
+          )}
+          {userAnswer?.correctAnswer !== answer &&
+            userAnswer?.answer !== answer && (
+              <ButtonAnswer
+                style={{
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+                disabled={userAnswer ? true : false}
+                value={answer}
+                onClick={callback}
+              >
+                <span dangerouslySetInnerHTML={{ __html: answer }} />
+              </ButtonAnswer>
+            )}
+        </div>
+      ))}
+    </div>
+  </QuestionContainer>
+);
+export default QuestionCard;
